4b3a27c18421fda7d6afc82b64533a099e9d2c51
CasLinden <cas_linden@msn.com>
2023-04-25 20:18:32 +0900

    made the site title look nicer

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,17 +1,22 @@
-import '/src/css/header.css';
+import "/src/css/header.css";
+import train from "/src/assets/train.svg";
 
 export default function Header() {
-    const defaultCity = 'Tokyo'
-    const defaultLine = 'Yamanote'
+  const defaultCity = "Tokyo";
+  const defaultLine = "Yamanote";
   return (
     <header className="header">
-        <div className='site-title'>
-            <div>Line<span>Legend</span></div>
+      <div className="header-left">
+        <img className="header-train-image" src={train} alt="" />
+        <div className="site-title">
+            <div className="line">Line</div><div className="legend">Legend</div>
         </div>
-        <div className="header-right">
-            <div className="city">{defaultCity}</div>
-            <div className="line">{defaultLine}</div>
-        </div>
+      </div>
+
+      <div className="header-right">
+        <div className="city">{defaultCity}</div>
+        <div className="line">{defaultLine}</div>
+      </div>
     </header>
   );
 }
